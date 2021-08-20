f26dfc6deb8c03dbba2d9450c2fe2540ac13a6d3
Alex Tyler <alexander.liam.tyler@gmail.com>
2021-08-20 15:15:25 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+function byeCookie(id){
+    element = document.getElementById(id);
+    element.parentNode.removeChild(element);
+}
+
+function tooFarh(){
+    myElement1 = document.querySelector(".temp1");
+        myElement1.innerText = "75";
+        console.log(myElement1);
+    myElement2 = document.querySelector(".temp2");
+        myElement2.innerText = "65";
+        console.log(myElement1);
+    myElement3 = document.querySelector(".temp3");
+        myElement3.innerText = "80";
+        console.log(myElement1);
+    myElement4 = document.querySelector(".temp4");
+        myElement4.innerText = "66";
+        console.log(myElement1);
+    myElement5 = document.querySelector(".temp5");
+        myElement5.innerText = "69";
+        console.log(myElement1);
+    myElement6 = document.querySelector(".temp6");
+        myElement6.innerText = "61";
+        console.log(myElement1);
+    myElement7 = document.querySelector(".temp7");
+        myElement7.innerText = "78";
+        console.log(myElement1);
+    myElement8 = document.querySelector(".temp8");
+        myElement8.innerText = "70";
+        console.log(myElement1);
+}
